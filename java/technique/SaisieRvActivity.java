eec1eda8d4e51fed6d016414d068169adbae7f02
mathis <138501804+dev-sio-M@users.noreply.github.com>
2024-03-08 17:37:54 +0100

    Create SaisieRvActivity.java

---
+++ java/technique/SaisieRvActivity.java
@@ -0,0 +1,17 @@
+package fr.gsb.rv.technique;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import fr.gsb.R;
+
+
+public class SaisieRvActivity extends AppCompatActivity {
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.saisie_rv_activity);
+   }
+}
