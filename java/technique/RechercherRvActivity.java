12b41d7bc33d99898e15f96eb91d316a6211fb3b
mathis <138501804+dev-sio-M@users.noreply.github.com>
2024-03-08 17:37:17 +0100

    Create RechercherRvActivity.java

---
+++ java/technique/RechercherRvActivity.java
@@ -0,0 +1,17 @@
+package fr.gsb.rv.technique;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import fr.gsb.R;
+
+
+public class RechercherRvActivity extends AppCompatActivity {
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.rechercher_rv_activity);
+   }
+}
